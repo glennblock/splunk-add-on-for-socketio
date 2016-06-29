b2f13039387089c280c667c1f4fde4831622f4ba
Glenn Block <glenn.block@gmail.com>
2016-06-29 13:19:50 -0700

    Updating to latest socket.io and Splunk SDK. Also fixing launch shell script

--- bin/socketio.sh
+++ bin/socketio.sh
@@ -1,3 +1,3 @@
 #!/bin/bash  
 current_dir=$(dirname "$0")
-exec "$SPLUNK_HOME/bin/splunk" cmd node "$current_dir/app/socketio_app.js"
+exec "$SPLUNK_HOME/bin/splunk" cmd node "$current_dir/app/socketio_app.js" $@
